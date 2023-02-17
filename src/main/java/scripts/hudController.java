099d68c8da92b45ea9cae1957f006e232097d73d
Colefreezer <Colefreezer1001@gmail.com>
2023-02-16 19:19:33 -0500

    Not too much
    not much shhh

---
+++ src/main/java/scripts/hudController.java
@@ -0,0 +1,21 @@
+package scripts;
+
+public class hudController {
+
+
+    public static void main(String[] args) {
+
+
+
+        HUD(true);
+    }
+
+
+        public static void HUD (boolean sw) {
+        while (sw == true){
+            System.out.println("Yep");
+
+        }
+
+    }
+}
